b6f7a11b592ee485dd625069848354dc91a62bf6
Goutham <goutham.atla@gmail.com>
2015-02-24 16:28:08 +0530

    a basic rna-seq data simulator

---
+++ simulate-rna-seq/generateFCdata.py
@@ -0,0 +1,81 @@
+import gzip
+import numpy,random
+
+foldchange_array=['10000,1','3000,10','2000,-10','200,+','200,-']
+true_fold_change=open("true_fc.txt","w")
+
+num_reads=100
+num_replicates=[3,3]
+dispersion=0.3
+
+
+transcriptome=gzip.open("101_transcriptome_chr1.fa.gz")
+transcript_list=[]
+transcript_dict={}
+fc_dict={}
+
+for line in transcriptome:
+    line=line.strip()
+    if line.startswith(">"):
+        transcript_list.append(line.replace(">",""))
+
+number_of_transcript = sum(int(item.split(",")[0]) for item in foldchange_array)
+numpy.random.shuffle(transcript_list)
+select_randome_transcripts=transcript_list[:number_of_transcript]
+
+
+line_count=0
+start=0
+end=0
+
+for item in range(len(foldchange_array)):
+    transcript_num=int(foldchange_array[item].split(",")[0])
+    numpy.random.shuffle(transcript_list)
+    fold_change=foldchange_array[item].split(",")[1]
+    end+=transcript_num
+    
+    for transcript in select_randome_transcripts[start:end]:
+        fc_dict[transcript]=fold_change
+        true_fold_change.write(str(transcript+"\t"+fold_change))
+        true_fold_change.write("\n")
+    
+        line_count+=1
+    start += end-start
+
+
+def get_NB(transcript,fc):
+    if fc=="+":
+        #The transcript is expressed only in first condition
+        condition1=numpy.random.negative_binomial(num_reads, dispersion,num_replicates[0])
+        condition2=[0 for x in range(num_replicates[1])]
+        counts=list(condition1)+condition2
+        print transcript+"\t"+"\t".join(str(v) for v in counts)
+    elif fc=="-":
+        #The transcript is expressed only in first condition
+        condition1=numpy.random.negative_binomial(num_reads, dispersion,num_replicates[0])
+        condition2=["0" for x in range(num_replicates[1])]
+        counts=list(condition1)+condition2
+        print transcript+"\t"+"\t".join(str(v) for v in counts)
+    elif int(fc)==1:
+        condition1=numpy.random.negative_binomial(num_reads, dispersion,num_replicates[0])
+        condition2=numpy.random.negative_binomial(num_reads, dispersion,num_replicates[1])
+        counts=list(condition1)+list(condition2)
+        print transcript+"\t"+"\t".join(str(v) for v in counts)
+    elif int(fc)>1:
+        condition1=numpy.random.negative_binomial(num_reads*int(fc), dispersion,num_replicates[0])
+        condition2=numpy.random.negative_binomial(num_reads, dispersion,num_replicates[1]) 
+        counts=list(condition1)+list(condition2)
+        print transcript+"\t"+"\t".join(str(v) for v in counts)
+    elif int(fc)<0:
+        condition1=numpy.random.negative_binomial(num_reads, 0.3,num_replicates[0])
+        condition2=numpy.random.negative_binomial(num_reads*abs(int(fc)), 0.3,num_replicates[1]) 
+        counts=list(condition1)+list(condition2)
+        print transcript+"\t"+"\t".join(str(v) for v in counts)        
+
+
+
+for key,value in fc_dict.items():
+    if isinstance(value,int):
+        get_NB(key,int(value))
+    else:
+        get_NB(key,str(value))
---
+++ simulate-rna-seq/getFastaReads.py
@@ -0,0 +1,83 @@
+import gzip,numpy
+from Bio import SeqIO
+from single_num import insert_length
+
+
+read_count_matrix = open("transcript_data_sim1.txt","r")
+
+number_of_samples=6
+
+transcript_dict={}
+
+for seq_record in SeqIO.parse(gzip.open("101_transcriptome_chr1.fa.gz"), "fasta"):
+    transcript_dict[seq_record.id]=seq_record.seq
+
+def single_num(mean,sd):
+    num = None
+    while True:
+        loc = mean
+        scale = sd
+        num = numpy.random.normal(loc=loc,scale=scale)
+
+        if True:
+            break
+
+    return num
+
+def generate_fasta(sample_name,count,transcript_id,read1,read2):
+    sample_R1=open("sample_"+sample_name+"_R1.fastq","a")
+    sample_R1.write(">"+"sample_"+sample_name+"_"+transcript_id+"_"+str(count)+"/1")
+    sample_R1.write("\n")
+    sample_R1.write(read1)
+    sample_R1.write("\n")
+    sample_R2=open("sample_"+sample_name+"_R2.fastq","a")
+    sample_R2.write(">"+"sample_"+sample_name+"_"+transcript_id+"_"+str(count)+"/1")
+    sample_R2.write("\n")
+    sample_R2.write(read1)
+    sample_R2.write("\n")
+
+
+frag_len=open("frag_len.txt","w")
+
+
+def get_NormallyDistributedInsertLengths(sample_name,transcript_name,copy_number):
+    
+    transcript_length=len(transcript_dict[transcript_name])
+    transcript_seq=transcript_dict[transcript_name]
+    
+    if transcript_length>202:
+        
+        insert_length=[int(x) for x in numpy.random.normal(loc=50,scale=20,size=copy_number)]        
+        
+        count=0
+        for insert_len in insert_length:
+            break_point=numpy.random.randint(0, high=(transcript_length-202))
+            fragment=(transcript_seq[break_point:break_point+insert_len+202])
+            frag_len.write(str(len(fragment))+"\n")
+            read1=fragment[0:101]
+            read2=fragment[-101:]
+            generate_fasta(str(1),count,transcript_name, str(read1), str(read2))
+            count+=1
+        
+    elif transcript_length<=202:
+        count=0
+        for insert_len in range(copy_number):
+            break_point=numpy.random.randint(0, high=transcript_length-101)
+            fragment=(transcript_seq[break_point:])
+            frag_len.write(str(len(fragment))+"\n")
+            read1=fragment[0:101]
+            read2=fragment[-101:]
+            generate_fasta(str(1),count, transcript_name, str(read1),str(read2))
+            count+=1
+            
+for sample in range(1,7):
+    sample_name="Sample_"+str(sample)    
+    
+    for transcript in read_count_matrix:
+        transcript=transcript.strip()
+        transcripts_matrix=transcript.split("\t")    
+        transcript_name=transcripts_matrix[0]
+        transcript_count=transcripts_matrix[int(sample)]
+        #print sample_name,transcript_name,len(transcript_dict[transcript_name]),transcript_count
+        get_NormallyDistributedInsertLengths(sample,transcript_name,int(transcript_count))
+    read_count_matrix.seek(0)
